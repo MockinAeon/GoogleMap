2c5732d8c9a6c53a35aaeb7f762ad7e22f04dbcb
Ubuntu <ubuntu@ip-172-31-3-158.us-west-2.compute.internal>
2017-10-23 18:08:17 +0000

    add jq and make it full screen

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname + '/templates/mymap.html'));
 });
 app.use('/css', express.static('css'));
-app.use('js', express.static('js'));
+app.use('/js', express.static('js'));
 app.listen(PORT_NUMBER, function () {
   console.log('Example app listening on port ' + PORT_NUMBER + ' !')
 });
